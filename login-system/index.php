3d641315d5d4bde70e1701d02c839814bfa78f72
Kyle M. Brown <mayvik@gmail.com>
2023-09-15 14:15:44 -0400

    Added - Logged in user content

--- login-system/index.php
+++ login-system/index.php
@@ -5,6 +5,11 @@
 <section>
 
 <p>Start Body content.</p>
+        <?php
+        if (isset($_SESSION["useruid"])){ // Check if a session exists with a useruid. If so, display profile and log out options on nav
+            echo "<h1>Hello there " . $_SESSION["useruid"] . "</h1>";
+        }
+        ?>
         <h1>This is an Research and Development for PHP</h1>
           <p>The objective is to practice coding with an actual project while documenting the process to internalize the knowledge.</p>
         <p>End body content.</p>
